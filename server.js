5fe2849e132173e15db6930b8b35053f5a83f412
Aakash <iakashman8653@gmail.com>
2023-11-25 16:24:49 +0530

    Till add to cart functionality

--- server.js
+++ server.js
@@ -1,33 +1,52 @@
+require("dotenv").config();
 const express=require("express");
 const app=express();
 const ejs=require("ejs");
 const path=require("path");
 const expressLayout=require("express-ejs-layouts");
 const PORT=process.env.PORT || 3000;
+const mongoose=require("mongoose");
+const session=require("express-session");
+const flash=require("express-flash");
+const MongoDbStore=require("connect-mongo");
 
+//Database Connection
+mongoose.connect("mongodb://127.0.0.1:27017/PizzaApp").then((data)=>{
+    console.log("Database connected");
+})
+
+
+//Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave:false,
+    saveUninitialized:false,
+    store: MongoDbStore.create({
+        mongoUrl: 'mongodb://127.0.0.1:27017/PizzaApp'
+      }),
+    cookie:{maxAge:1000*60*60*24}
+}))
+
+app.use(flash());
+
+//Assests
+app.use(express.static("public"))
+app.use(express.json())
+
+//Global middleware
+app.use((req,res,next)=>{
+    res.locals.session=req.session
+    next()
+})
 //set template engine
 app.use(expressLayout);
 app.set("views",path.join(__dirname,"/resources/views"));
 app.set("view engine","ejs");
 
 
-//Assests
-app.use(express.static("public"))
-app.get("/",(req,res)=>{
-    res.render("home")
-})
-
-app.get("/cart",(req,res)=>{
-    res.render("customers/cart");
-})
+require("./routes/web")(app)
 
 
-app.get("/login",(req,res)=>{
-    res.render("auth/login");
-})
-app.get("/register",(req,res)=>{
-    res.render("auth/register");
-})
 
 
 app.listen(PORT,(err)=>{
